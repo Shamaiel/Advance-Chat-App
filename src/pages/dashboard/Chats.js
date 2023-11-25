a2325b7fc4742257eb728ac1b35f89b25628fd9e
Shamaiel <mswani258@gmail.com>
2023-11-25 11:12:13 +0530

    avatar added

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,9 +1,77 @@
-import { Box, Button, Divider, IconButton, Stack, Typography } from '@mui/material'
-import React from 'react'
-import {CircleDashed, MagnifyingGlass, ArchiveBox} from "phosphor-react"
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { CircleDashed, MagnifyingGlass, ArchiveBox } from "phosphor-react";
 import { styled, alpha } from "@mui/material/styles";
 import { InputBase } from "@mui/material";
+import { faker } from "@faker-js/faker";
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+const ChatElement = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 60,
+        borderRadius: 1,
+        backgroundColor: "#fff",
+      }}
+      p={2}
+    >
+      <Stack
+        direction="row"
+        alignItems={"Center"}
+        justifyContent="space-between"
+      >
+        <Stack>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar src={faker.image.avatar()} />
+          </StyledBadge>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -14,33 +82,25 @@ const Search = styled("div")(({ theme }) => ({
   width: "100%",
 }));
 
-
-
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
-
-  
-  
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-      color: "inherit",
-      "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        width: "100%",
-      },
-    }));
-  
-  
-
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
 
 const Chats = () => {
   return (
@@ -50,48 +110,45 @@ const Chats = () => {
         height: "100vh",
         width: 320,
         backgroundColor: "#eef1f8",
-        boxShadow: "0px 0px 2px rgba(0 0 0 0.25)"
-        
+        boxShadow: "0px 0px 2px rgba(0 0 0 0.25)",
       }}
     >
-        <Stack p={3} spacing={2}>
-        <Stack direction="row" alignItems={"center"} justifyContent="space-between" >
-            <Typography color="black" variant="h5">Chats</Typography>
-            <IconButton>
-             <CircleDashed/>
-            </IconButton>
-
+      <Stack p={3} spacing={2}>
+        <Stack
+          direction="row"
+          alignItems={"center"}
+          justifyContent="space-between"
+        >
+          <Typography color="black" variant="h5">
+            Chats
+          </Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
         </Stack>
-          <Stack sx={{width:"100%"}} >
-           <Search>
+        <Stack sx={{ width: "100%" }}>
+          <Search>
             <SearchIconWrapper>
-                <MagnifyingGlass color="#709CE6"/>
+              <MagnifyingGlass color="#709CE6" />
             </SearchIconWrapper>
             <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-               
-              />
-           </Search>
-           <Stack spacing={1}>
-           <Stack direction="row" alignItems={"center"} spacing={1.5}>
-             <ArchiveBox size={24}/>
-             <Button>Archive</Button>
-           
-             </Stack>
-             <Divider/>
-
-           </Stack>
-            
-            
-
-
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Stack spacing={1}>
+            <Stack direction="row" alignItems={"center"} spacing={1.5}>
+              <ArchiveBox size={24} />
+              <Button>Archive</Button>
+            </Stack>
+            <Divider />
           </Stack>
-        </Stack>
-       
-     
-    </Box>
-  )
-}
 
-export default Chats
+          <ChatElement />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Chats;
